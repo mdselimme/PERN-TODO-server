7c4c804bd6e8049666eee7ede938b68078959b46
mdselimme <mdselimdev@gmail.com>
2025-03-05 07:11:59 +0600

    add update todo put method

--- index.js
+++ index.js
@@ -55,7 +55,17 @@ app.get("/todos/:id", async (req, res) => {
 
 // update a todo 
 app.put("/todos/:id", async (req, res) => {
-
+    try {
+        const { id } = req.params;
+        const { description } = req.body;
+        const updateTodo = await pool.query(
+            "UPDATE todo SET description = $1 WHERE todo_id = $2",
+            [description, id]
+        );
+        res.json("Todo is Updated");
+    } catch (error) {
+        console.error(error.message);
+    }
 })
 
 
